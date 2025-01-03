31f96d97ad3898750f95a224eee36ece8551947a
Levindu Herath <levinduherath@gmail.com>
2025-01-03 23:32:17 +0530

    create account page complete

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/create.dart';
 import 'package:mobile/home.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Home(),
+    home: Create(),
   ));
 }
