b50941b0f7e445696d547ec2faa3a9ed6e0ac40c
Levindu Herath <levinduherath@gmail.com>
2025-01-05 00:03:40 +0530

    Student sign in page completed

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/create.dart';
+import 'package:mobile/createstudent.dart';
 import 'package:mobile/home.dart';
 import 'package:mobile/roleselection.dart';
 
@@ -7,6 +8,6 @@ import 'otpinput.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Otpinput(),
+    home: Createstudent(),
   ));
 }
