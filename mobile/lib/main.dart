1f72dff4e5c7177716f58976c7a510b8faff778b
Levindu Herath <levinduherath@gmail.com>
2025-01-07 00:55:23 +0530

    timer UI structure created

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:mobile/aboutsc1.dart';
 import 'package:mobile/create.dart';
 import 'package:mobile/createparent.dart';
@@ -9,9 +8,8 @@ import 'package:mobile/createstudent2.dart';
 import 'package:mobile/home.dart';
 import 'package:mobile/login.dart';
 import 'package:mobile/roleselection.dart';
+import 'package:mobile/timer.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Home(),
-  ));
+  runApp(const PomodoroApp());
 }
