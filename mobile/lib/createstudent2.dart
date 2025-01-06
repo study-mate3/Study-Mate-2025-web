876c943696a9b152b96700c31b22ffe60f7ec7a9
Levindu Herath <levinduherath@gmail.com>
2025-01-06 20:01:17 +0530

    sign in UI completed

--- mobile/lib/createstudent2.dart
+++ mobile/lib/createstudent2.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/createstudent2.dart';
 
-class Createstudent extends StatefulWidget {
-  const Createstudent({super.key});
+class Createstudent2 extends StatefulWidget {
+  const Createstudent2({super.key});
 
   @override
-  State<Createstudent> createState() => _CreatestudentState();
+  State<Createstudent2> createState() => _Createstudent2State();
 }
 
-class _CreatestudentState extends State<Createstudent> {
+class _Createstudent2State extends State<Createstudent2> {
   String? selectedOption;
   @override
   Widget build(BuildContext context) {
@@ -24,7 +23,7 @@ class _CreatestudentState extends State<Createstudent> {
                   width: 148,
                 ),
               ),
-              const SizedBox(height: 40),
+              const SizedBox(height: 60),
               Container(
                 child: const Text(
                   'Create Your Account',
@@ -38,7 +37,7 @@ class _CreatestudentState extends State<Createstudent> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     const Text(
-                      'User Name',
+                      'Email',
                       style:
                           TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
@@ -49,22 +48,54 @@ class _CreatestudentState extends State<Createstudent> {
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(15),
                         ), // Add a border around the input
-                        hintText: 'Enter Your User Name', // Placeholder text
+                        hintText: 'Enter Your Email', // Placeholder text
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 40),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Password",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                        height: 8), // Spacing between label and input
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ), // Add a border around the input
+                        hintText: "Enter Your Password", // Placeholder text
                       ),
                     ),
-                    const SizedBox(height: 40),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
                     Container(
                         child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         const Text(
-                          'Select Your Grade or Education Level',
+                          'Select Your Gender',
                           style: TextStyle(
                               fontSize: 18, fontWeight: FontWeight.bold),
                         ),
                         RadioListTile<String>(
-                          title: const Text('Grade 6-9'),
-                          value: 'Grade 6-9',
+                          title: const Text('Male'),
+                          value: 'Male',
                           groupValue: selectedOption,
                           onChanged: (value) {
                             setState(() {
@@ -73,28 +104,8 @@ class _CreatestudentState extends State<Createstudent> {
                           },
                         ),
                         RadioListTile<String>(
-                          title: const Text('Grade 10-11'),
-                          value: 'Grade 10-11',
-                          groupValue: selectedOption,
-                          onChanged: (value) {
-                            setState(() {
-                              selectedOption = value;
-                            });
-                          },
-                        ),
-                        RadioListTile<String>(
-                          title: const Text('Grade 12-13'),
-                          value: 'Grade 12-13',
-                          groupValue: selectedOption,
-                          onChanged: (value) {
-                            setState(() {
-                              selectedOption = value;
-                            });
-                          },
-                        ),
-                        RadioListTile<String>(
-                          title: const Text('University'),
-                          value: 'University',
+                          title: const Text('Female'),
+                          value: 'Female',
                           groupValue: selectedOption,
                           onChanged: (value) {
                             setState(() {
@@ -112,12 +123,7 @@ class _CreatestudentState extends State<Createstudent> {
                   child: SizedBox(
                 width: 250,
                 child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const Createstudent2()));
-                  },
+                  onPressed: () {},
                   style: ElevatedButton.styleFrom(
                     backgroundColor: const Color(0xFF0570B2),
                     padding: const EdgeInsets.all(5),
@@ -125,7 +131,7 @@ class _CreatestudentState extends State<Createstudent> {
                       borderRadius: BorderRadius.circular(25),
                     ),
                   ),
-                  child: const Text('Next',
+                  child: const Text('Create Account',
                       style: TextStyle(
                           color: Colors.white,
                           fontSize: 17,
