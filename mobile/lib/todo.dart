74e2f43af52099b2d51ff7c91004b13ef5c11e98
Levindu Herath <levinduherath@gmail.com>
2025-01-10 23:55:27 +0530

    todo list page updated

--- mobile/lib/todo.dart
+++ mobile/lib/todo.dart
@@ -13,19 +13,42 @@ class _TodoState extends State<Todo> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const SizedBox(height: 150, width: 20),
-          Container(
-            child:const Text('Menu'),
-          ),
-          const SizedBox(width: 200),
-          Container(
-            child: Image.asset('assets/img/logo2.png', height: 40, width: 140),
-          ),
-        ],
-      )),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(height: 150, width: 20),
+                Container(
+                  child: const Text('Menu'),
+                ),
+                const SizedBox(width: 200),
+                Container(
+                  child: Image.asset('assets/img/logo2.png',
+                      height: 40, width: 140),
+                ),
+              ],
+            ),
+            //const SizedBox(height: 20),
+            Container(
+              child: const Text(
+                'Manage your To-Do List',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Container(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('+ Add New Task',
+                    style: TextStyle(fontSize: 18)),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
