9437bd170cafd10e73faeb49a14f7a8e53a84396
tharuNethuu <tharu0nethu@gmail.com>
2025-12-07 15:59:51 +0530

    updated port 0 0 0 0

--- server/server.js
+++ server/server.js
@@ -107,4 +107,6 @@ Keep feedback friendly and motivational.
 }); 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+})
